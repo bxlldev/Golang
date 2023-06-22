2ab06cb23234d7f8ef318ae9982b3a82d5a0412a
bxlldev <127035655+bxlldev@users.noreply.github.com>
2023-06-22 22:30:14 +0700

    Various declaration of variables

--- Learning/main.go
+++ Learning/main.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+//var price = 100
+
 func main() {
-	fmt.Println("Hello Ball's World")
+
+	msg := "Hello Ball's World"
+	msg = "Thanachart Saejueng" + " Haha"
+	//var age int = 23
+	//var price float64 = 34.1414
+	//var check bool = true
+
+	//age := 23
+	//price := 34.1414
+	//check := true
+
+	//var age, price, check = 23, 34.1414, true
+
+	age, price, check := 23, 34.1414, true
+
+	fmt.Println("msg:", msg)
+	fmt.Println("age: ", age)
+	fmt.Println("price: ", price)
+	fmt.Println("check: ", check)
 }
